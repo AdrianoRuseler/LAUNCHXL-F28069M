0324b3ecc2a8836f4b0043eb3532c585713622f6
Adriano Ruseler <adrianoruseler@gmail.com>
2016-08-23 10:45:20 -0300

    Adicionado função que copia para RAM na gravação em FLASH

--- Example_2806xLEDBlink/Example_2806xLEDBlink.c
+++ Example_2806xLEDBlink/Example_2806xLEDBlink.c
@@ -21,100 +21,150 @@
 //             http://www.ti.com/ ALL RIGHTS RESERVED $
 //###########################################################################
 
-
+#include "Example_2806xLEDBlink.h"
 #include "DSP28x_Project.h"     // Device Headerfile and Examples Include File
+#include "C28x_FPU_FastRTS.h"
+#include <math.h>
+
+#if FLASH //
+// Functions that will be run from RAM need to be assigned to
+// a different section.  This section will then be mapped using
+// the linker cmd file.
+
+void MemCopy(Uint16 *SourceAddr, Uint16* SourceEndAddr, Uint16* DestAddr);
+
+#pragma CODE_SECTION(cpu_timer0_isr, "ramfuncs");
+
+// These are defined by the linker
+extern Uint16 RamfuncsLoadStart;
+extern Uint16 RamfuncsLoadEnd;
+extern Uint16 RamfuncsRunStart;
+extern Uint16 RamfuncsLoadSize;
+#endif
+
 
 // Prototype statements for functions found within this file.
 __interrupt void cpu_timer0_isr(void);
 
+
 void main(void)
 {
 
-// Step 1. Initialize System Control:
-// PLL, WatchDog, enable Peripheral Clocks
-// This example function is found in the F2806x_SysCtrl.c file.
-   InitSysCtrl();
+	// Step 1. Initialize System Control:
+	// PLL, WatchDog, enable Peripheral Clocks
+	// This example function is found in the F2806x_SysCtrl.c file.
+	InitSysCtrl();
 
-// Step 2. Initalize GPIO:
-// This example function is found in the F2806x_Gpio.c file and
-// illustrates how to set the GPIO to it's default state.
-// InitGpio();  // Skipped for this example
-
-
-// Step 3. Clear all interrupts and initialize PIE vector table:
-// Disable CPU interrupts
-   DINT;
-
-// Initialize the PIE control registers to their default state.
-// The default state is all PIE interrupts disabled and flags
-// are cleared.
-// This function is found in the F2806x_PieCtrl.c file.
-   InitPieCtrl();
-
-// Disable CPU interrupts and clear all CPU interrupt flags:
-   IER = 0x0000;
-   IFR = 0x0000;
-
-// Initialize the PIE vector table with pointers to the shell Interrupt
-// Service Routines (ISR).
-// This will populate the entire table, even if the interrupt
-// is not used in this example.  This is useful for debug purposes.
-// The shell ISR routines are found in F2806x_DefaultIsr.c.
-// This function is found in F2806x_PieVect.c.
-   InitPieVectTable();
-
-// Interrupts that are used in this example are re-mapped to
-// ISR functions found within this file.
-   EALLOW;  // This is needed to write to EALLOW protected registers
-   PieVectTable.TINT0 = &cpu_timer0_isr;
-   EDIS;    // This is needed to disable write to EALLOW protected registers
-
-// Step 4. Initialize the Device Peripheral. This function can be
-//         found in F2806x_CpuTimers.c
-   InitCpuTimers();   // For this example, only initialize the Cpu Timers
-// Configure CPU-Timer 0 to interrupt every 500 milliseconds:
-// 80MHz CPU Freq, 50 millisecond Period (in uSeconds)
-   ConfigCpuTimer(&CpuTimer0, 80, 500000);
+	// Step 2. Initalize GPIO:
+	// This example function is found in the F2806x_Gpio.c file and
+	// illustrates how to set the GPIO to it's default state.
+	// InitGpio();  // Skipped for this example
 
 
-// To ensure precise timing, use write-only instructions to write to the entire register. Therefore, if any
-// of the configuration bits are changed in ConfigCpuTimer and InitCpuTimers (in F2806x_CpuTimers.h), the
-// below settings must also be updated.
+	// Step 3. Clear all interrupts and initialize PIE vector table:
+	// Disable CPU interrupts
+	DINT;
 
-   CpuTimer0Regs.TCR.all = 0x4001; // Use write-only instruction to set TSS bit = 0
+	// Initialize the PIE control registers to their default state.
+	// The default state is all PIE interrupts disabled and flags
+	// are cleared.
+	// This function is found in the F2806x_PieCtrl.c file.
+	InitPieCtrl();
 
-// Step 5. User specific code, enable interrupts:
+	// Disable CPU interrupts and clear all CPU interrupt flags:
+	IER = 0x0000;
+	IFR = 0x0000;
 
-// Configure GPIO34 as a GPIO output pin
-   EALLOW;
-   GpioCtrlRegs.GPBMUX1.bit.GPIO34 = 0;
-   GpioCtrlRegs.GPBDIR.bit.GPIO34 = 1;
-   EDIS;
+	// Initialize the PIE vector table with pointers to the shell Interrupt
+	// Service Routines (ISR).
+	// This will populate the entire table, even if the interrupt
+	// is not used in this example.  This is useful for debug purposes.
+	// The shell ISR routines are found in F2806x_DefaultIsr.c.
+	// This function is found in F2806x_PieVect.c.
+	InitPieVectTable();
 
-// Enable CPU INT1 which is connected to CPU-Timer 0:
-   IER |= M_INT1;
+	// Interrupts that are used in this example are re-mapped to
+	// ISR functions found within this file.
+	EALLOW;  // This is needed to write to EALLOW protected registers
+	PieVectTable.TINT0 = &cpu_timer0_isr;
+	EDIS;    // This is needed to disable write to EALLOW protected registers
 
-// Enable TINT0 in the PIE: Group 1 interrupt 7
-   PieCtrlRegs.PIEIER1.bit.INTx7 = 1;
+	// Step 4. Initialize the Device Peripheral. This function can be
+	//         found in F2806x_CpuTimers.c
+	InitCpuTimers();   // For this example, only initialize the Cpu Timers
+	// Configure CPU-Timer 0 to interrupt every 500 milliseconds:
+	// 80MHz CPU Freq, 50 millisecond Period (in uSeconds)
+	ConfigCpuTimer(&CpuTimer0, 80, 500000);
 
-// Enable global Interrupts and higher priority real-time debug events:
-   EINT;   // Enable Global interrupt INTM
-   ERTM;   // Enable Global realtime interrupt DBGM
 
-// Step 6. IDLE loop. Just sit and loop forever (optional):
-   for(;;);
+	// To ensure precise timing, use write-only instructions to write to the entire register. Therefore, if any
+	// of the configuration bits are changed in ConfigCpuTimer and InitCpuTimers (in F2806x_CpuTimers.h), the
+	// below settings must also be updated.
+
+	CpuTimer0Regs.TCR.all = 0x4001; // Use write-only instruction to set TSS bit = 0
+
+	// Step 5. User specific code, enable interrupts:
+
+	// Configure GPIO34 as a GPIO output pin
+	EALLOW;
+	GpioCtrlRegs.GPBMUX1.bit.GPIO34 = 0;
+	GpioCtrlRegs.GPBDIR.bit.GPIO34 = 1;
+	EDIS;
+
+	// Enable CPU INT1 which is connected to CPU-Timer 0:
+	IER |= M_INT1;
+
+	// Enable TINT0 in the PIE: Group 1 interrupt 7
+	PieCtrlRegs.PIEIER1.bit.INTx7 = 1;
+
+	// Enable global Interrupts and higher priority real-time debug events:
+	EINT;   // Enable Global interrupt INTM
+	ERTM;   // Enable Global realtime interrupt DBGM
+
+
+#if FLASH
+	MemCopy(&RamfuncsLoadStart, &RamfuncsLoadEnd, &RamfuncsRunStart);
+
+	// Call Flash Initialization to setup flash waitstates
+	// This function must reside in RAM
+	InitFlash();
+#endif
+
+	// Step 6. IDLE loop. Just sit and loop forever (optional):
+	for(;;);
 }
 
 
 __interrupt void cpu_timer0_isr(void)
 {
-   CpuTimer0.InterruptCount++;
-   GpioDataRegs.GPBTOGGLE.bit.GPIO34 = 1; // Toggle GPIO34 once per 500 milliseconds
-   // Acknowledge this interrupt to receive more interrupts from group 1
-   PieCtrlRegs.PIEACK.all = PIEACK_GROUP1;
+	CpuTimer0.InterruptCount++;
+	GpioDataRegs.GPBTOGGLE.bit.GPIO34 = 1; // Toggle GPIO34 once per 500 milliseconds
+
+	/*
+	trig.wt+=temp.t2f*TS_PLL;
+
+	if(trig.wt>PIX1) trig.wt = trig.wt - PIX2;
+
+	sincos(trig.wt, &(trig.senoA), &(trig.coseA));
+	sincos((trig.wt-2.09439510239319549230), &(trig.senoB), &(trig.coseB));
+	sincos((trig.wt+2.09439510239319549230), &(trig.senoC), &(trig.coseC));
+	 */
+
+
+	// Acknowledge this interrupt to receive more interrupts from group 1
+	PieCtrlRegs.PIEACK.all = PIEACK_GROUP1;
 }
 
-
+#if FLASH //
+void MemCopy(Uint16 *SourceAddr, Uint16* SourceEndAddr, Uint16* DestAddr)
+{
+	while(SourceAddr < SourceEndAddr)
+	{
+		*DestAddr++ = *SourceAddr++;
+	}
+	return;
+}
+#endif
 //===========================================================================
 // No more.
 //===========================================================================
